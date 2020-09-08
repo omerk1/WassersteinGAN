9c501376502931c6f04d071fd0ecbcc42c1090d7
Omer Koren <omerk@playtika.com>
2020-09-08 16:12:37 +0300

    GAN omit BN

--- gan.py
+++ gan.py
@@ -79,25 +79,28 @@ class Generator(nn.Module):
         num_generated_features = 128
         self.de_conv = nn.Sequential(
 
-            nn.ConvTranspose2d(z_dim, num_generated_features * 8, kernel_size=featuremap_size, stride=1, padding=0, bias=False),
+            nn.ConvTranspose2d(z_dim, num_generated_features * 8, kernel_size=featuremap_size, stride=1,
+                               padding=0, bias=False),
             nn.BatchNorm2d(num_generated_features * 8),
             nn.ReLU(inplace=True),
 
-            nn.ConvTranspose2d(num_generated_features * 8, num_generated_features * 4, kernel_size=4, stride=2, padding=1,
-                               bias=False),
+            nn.ConvTranspose2d(num_generated_features * 8, num_generated_features * 4, kernel_size=4, stride=2,
+                               padding=1, bias=False),
             # nn.BatchNorm2d(num_generated_features * 4),
             nn.ReLU(inplace=True),
 
-            nn.ConvTranspose2d(num_generated_features * 4, num_generated_features * 2, kernel_size=4, stride=2, padding=1,
-                               bias=False),
+            nn.ConvTranspose2d(num_generated_features * 4, num_generated_features * 2, kernel_size=4, stride=2,
+                               padding=1, bias=False),
             # nn.BatchNorm2d(num_generated_features * 2),
             nn.ReLU(inplace=True),
 
-            nn.ConvTranspose2d(num_generated_features * 2, num_generated_features, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.ConvTranspose2d(num_generated_features * 2, num_generated_features, kernel_size=4, stride=2,
+                               padding=1, bias=False),
             # nn.BatchNorm2d(num_generated_features),
             nn.ReLU(inplace=True),
 
-            nn.ConvTranspose2d(num_generated_features, out_channels, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.ConvTranspose2d(num_generated_features, out_channels, kernel_size=4, stride=2,
+                               padding=1, bias=False),
             nn.Tanh()
 
         )
